069a832f28985bcdb9b7e13fefed6ef8a5fb5222
Josh Storm <joshua_taljaard@outlook.com>
2018-05-21 15:57:20 +0100

    working on Item & Weapon pickup with WASD keys

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,7 @@
 
 console.log('Welcome');
 
-//MAP//
+//###########################MAP##############################//
 const gameGrid = [
   [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
   [1,2,0,0,0,0,0,0,0,4,1,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,1,1],
@@ -25,30 +25,38 @@ const gameGrid = [
   [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 ];
 
-//X & Y DIV INFO//
+//#######################X & Y INFO#############################//
 $(()=>{
   $('#map').on('mouseover','div',function(){
     $('#cell-address').val(`${$(this).data('x')}-${$(this).data('y')}`);
   });
   let playerChar = {};
-  //GRID LAYOUT//
+
+  //###################GRID LAYOUT#########################//
   $.each(gameGrid, (i, row) =>{
     $.each(row, (j, cell) =>{
       const $element = $('<div />');
+      //Path//
       if(cell === 0){
         $element.addClass('path');
+        //Wall//
       }if(cell === 1){
         $element.addClass('wall');
+        //Player//
       }if(cell === 2){
         $element.addClass('player');
         playerChar = {x: i, y: j};
         gameGrid[playerChar.x][playerChar.y]= 0;
+        //Mob//
       }if(cell === 3){
         $element.addClass('mob');
+        //Weapon//
       }if(cell === 4){
         $element.addClass('weapon');
+        //Health Potion//
       }if(cell === 5){
         $element.addClass('healthPot');
+        //Boss Enemy//
       }else if(cell === 6){
         $element.addClass('boss');
       }
@@ -60,9 +68,11 @@ $(()=>{
       $element.appendTo('#map');
     });
   });
+  //###############ITEMS & WEAPONS PICKUP##########################//
+  function pickupWeapon(){
 
-
-  //PLAYER MOVEMENT//
+  }
+  //#####################PLAYER MOVEMENT################################//
 
   function playerMovement(){
 
@@ -72,53 +82,49 @@ $(()=>{
     $(document).on('keypress', function(e){
       switch(e.which){
         case 119:     // MOVE PLAYER UP//
-        if (gameGrid[playerChar.x-1][playerChar.y] === 0){
-          $('.player').removeClass('player').addClass('path');
-          playerChar.x -= 1;
-          $(`div[data-x='${playerChar.x}'][data-y='${playerChar.y}']`).removeClass('path').addClass('player');
-        }
-        // if (gameGrid[playerLocation.x][playerLocation.y] === 4){
-        //     pickupWeapon();
-        // if (gameGrid[playerLocation.x][playerLocation.y] === 5){
-        //     pickupHealth();
-        //   }
-        break;
+          if (gameGrid[playerChar.x-1][playerChar.y] === 0){
+            $('.player').removeClass('player').addClass('path');
+            playerChar.x -= 1;
+            $(`div[data-x='${playerChar.x}'][data-y='${playerChar.y}']`).removeClass('path').addClass('player');
+          }if (gameGrid[playerChar.x][playerChar.y] === 4){
+            pickupWeapon();
+          }if (gameGrid[playerChar.x][playerChar.y] === 5){
+            pickupHealth();
+          }
+          break;
         case 97:     // MOVE PLAYER LEFT//
-        if (gameGrid[playerChar.x][playerChar.y-1] === 0){
-          $('.player').removeClass('player').addClass('path');
-          playerChar.y -= 1;
-          $(`div[data-x='${playerChar.x}'][data-y='${playerChar.y}']`).removeClass('path').addClass('player');
-        }
-        // if (gameGrid[playerLocation.x][playerLocation.y] === 4){
-        //     pickupWeapon();
-        // if (gameGrid[playerLocation.x][playerLocation.y] === 5){
-        //     pickupHealth();
-        //   }
-        break;
+          if (gameGrid[playerChar.x][playerChar.y-1] === 0){
+            $('.player').removeClass('player').addClass('path');
+            playerChar.y -= 1;
+            $(`div[data-x='${playerChar.x}'][data-y='${playerChar.y}']`).removeClass('path').addClass('player');
+          }if (gameGrid[playerChar.x][playerChar.y] === 4){
+            pickupWeapon();
+          }if (gameGrid[playerChar.x][playerChar.y] === 5){
+            pickupHealth();
+          }
+          break;
         case 115:     // MOVE PLAYER DOWN//
-        if (gameGrid[playerChar.x+1][playerChar.y] === 0){
-          $('.player').removeClass('player').addClass('path');
-          playerChar.x += 1;
-          $(`div[data-x='${playerChar.x}'][data-y='${playerChar.y}']`).removeClass('path').addClass('player');
-        }
-        // if (gameGrid[playerLocation.x][playerLocation.y] === 4){
-        //     pickupWeapon();
-        // if (gameGrid[playerLocation.x][playerLocation.y] === 5){
-        //     pickupHealth();
-        //   }
-        break;
+          if (gameGrid[playerChar.x+1][playerChar.y] === 0){
+            $('.player').removeClass('player').addClass('path');
+            playerChar.x += 1;
+            $(`div[data-x='${playerChar.x}'][data-y='${playerChar.y}']`).removeClass('path').addClass('player');
+          }if (gameGrid[playerChar.x][playerChar.y] === 4){
+            pickupWeapon();
+          }if (gameGrid[playerChar.x][playerChar.y] === 5){
+            pickupHealth();
+          }
+          break;
         case 100:     // MOVE PLAYER RIGHT//
-        if (gameGrid[playerChar.x][playerChar.y+1] === 0){
-          $('.player').removeClass('player').addClass('path');
-          playerChar.y += 1;
-          $(`div[data-x='${playerChar.x}'][data-y='${playerChar.y}']`).removeClass('path').addClass('player');
-        }
-        // if (gameGrid[playerLocation.x][playerLocation.y] === 4){
-        //     pickupWeapon();
-        // if (gameGrid[playerLocation.x][playerLocation.y] === 5){
-        //     pickupHealth();
-        //   }
-        break;
+          if (gameGrid[playerChar.x][playerChar.y+1] === 0){
+            $('.player').removeClass('player').addClass('path');
+            playerChar.y += 1;
+            $(`div[data-x='${playerChar.x}'][data-y='${playerChar.y}']`).removeClass('path').addClass('player');
+          }if (gameGrid[playerChar.x][playerChar.y] === 4){
+            pickupWeapon();
+          }if (gameGrid[playerChar.x][playerChar.y] === 5){
+            pickupHealth();
+          }
+          break;
       };
     });
   }
