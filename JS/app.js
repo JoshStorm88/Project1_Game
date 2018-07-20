390ad29ebff57265fb0b5a3362996febba0ac4c9
Josh Storm <joshua_taljaard@outlook.com>
2018-07-20 10:29:24 +0100

    minor changes

---
+++ JS/app.js
@@ -0,0 +1,319 @@
+console.log('Fangthar begins his quest!');
+
+const gameGrid = [
+  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+  [1,2,0,0,0,0,0,0,0,4,1,1,1,1,0,6,0,0,0,0,0,1,1,1,1,1,1,1],
+  [1,0,0,0,0,0,0,0,0,1,1,1,1,1,5,0,0,1,1,1,0,3,1,0,4,1,1,1],
+  [1,0,0,0,0,3,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,1,1,1,1],
+  [1,0,0,1,1,0,0,0,0,0,0,0,1,1,5,1,0,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,1,1,0,0,0,3,1,1,1,1,0,0,0,0,0,0,0,0,0,3,0,0,0,1],
+  [1,0,3,0,1,0,0,0,0,0,0,0,0,1,1,0,0,3,0,0,0,0,0,0,0,0,0,1],
+  [1,0,1,0,0,0,0,0,0,1,1,1,0,3,1,1,0,1,1,0,0,0,1,0,1,1,0,1],
+  [1,0,1,1,1,0,0,3,0,1,1,4,0,0,1,0,4,0,1,1,3,0,1,0,0,1,1,1],
+  [1,4,0,0,0,0,0,1,0,0,1,1,1,5,1,1,1,1,1,1,1,1,1,0,0,1,1,1],
+  [1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,5,1,0,0,0,0,0,0,1,1,1,1],
+  [1,0,0,0,1,0,3,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,6,1],
+  [1,0,0,6,1,1,0,0,1,1,1,0,1,1,1,1,0,0,0,0,0,0,1,1,5,0,0,1],
+  [1,0,1,1,1,1,0,0,1,1,1,1,1,5,4,1,1,1,1,1,0,1,1,1,1,1,0,1],
+  [1,3,0,0,1,4,0,1,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1],
+  [1,0,0,1,1,1,0,0,0,0,1,0,1,1,1,1,0,1,0,1,1,1,1,1,1,0,0,1],
+  [1,0,0,0,0,0,0,1,1,1,1,0,1,1,0,0,0,1,0,1,0,0,0,0,1,3,0,1],
+  [1,5,1,0,1,1,1,1,1,0,3,0,0,1,0,1,0,3,0,1,0,1,0,1,1,1,0,1],
+  [1,1,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,1],
+  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+];
+
+//#######################X & Y LOCATIONS IN CONSOLE#############################//
+$(()=>{
+  $('#map').on('mouseover','div',function(){
+    $('#cell-address').val(`${$(this).data('x')}-${$(this).data('y')}`);
+  });
+
+  // ######################### PLAYER CHARACTER ###################################//
+
+  const playerChar = {health: 120, attack: 10};
+
+  //###################GRID CELL CLASSES#########################//
+  $.each(gameGrid, (i, row) =>{
+    $.each(row, (j, cell) =>{
+      const $element = $('<div />');
+      //Path//
+      if(cell === 0){
+        $element.addClass('path');
+        //Wall//
+      }if(cell === 1){
+        $element.addClass('wall');
+        //Player//
+      }if(cell === 2){
+        $element.addClass('player');
+        playerChar.x = i;
+        playerChar.y = j;
+        gameGrid[playerChar.x][playerChar.y]= 0;
+        //Mob//
+      }if(cell === 3){
+        $element.addClass('mob');
+        //Weapon//
+      }if(cell === 4){
+        $element.addClass('weapon');
+        //Health Potion//
+      }if(cell === 5){
+        $element.addClass('healthPot');
+        //Boss//
+      }if(cell === 6){
+        $element.addClass('boss');
+        //death//
+      }if(cell === 8){
+        $element.addClass('monsterSkeleton');
+      }
+      $element.attr('data-x',i);
+      $element.attr('data-y',j);
+      $element.on('click', function(){
+        console.log($(this).data());
+      });
+      $element.appendTo('#map');
+    });
+  });
+  //###############MOVEMENT,ITEMS & WEAPONS PICKUP FUNCTIONS & LOGS##########################//
+
+  const weaponArray = [
+    {name: 'Rusty Greatsword'},
+    {name: 'Chipped Mace'},
+    {name: 'Twisted Flail'},
+    {name: 'Smoldering Pike'},
+    {name: 'Moldy Ham Hock of suffering'},
+    {name: 'Unicorn Horn'},
+    {name: 'Big Pointy Stick'}
+  ];
+  const weaponName = weaponArray[Math.floor(Math.random()*weaponArray.length)].name;
+
+  function pickupWeapon(){
+    const weaponName = weaponArray[Math.floor(Math.random()*weaponArray.length)].name;
+    $(`div[data-x='${playerChar.x}'][data-y='${playerChar.y}']`).removeClass('weapon').addClass('player');
+    gameGrid[playerChar.x][playerChar.y] = 0;
+    weaponAudio.play();
+    $(playerChar.attack += (Math.floor(Math.random() * 20)));
+    $heroWeapon.text(weaponName);
+    $heroAttack.text('Attack' + playerChar.attack + 'DAMAGE');
+    $heroLog.text(`The chest opens, revealing a ${weaponName}! Fangthar now attacks with ${playerChar.attack} damage!`);
+  }
+  function pickupHealth(){
+    $(`div[data-x='${playerChar.x}'][data-y='${playerChar.y}']`).removeClass('healthPot').addClass('player');
+    gameGrid[playerChar.x][playerChar.y] = 0;
+    healthAudio.play();
+    $(playerChar.health += 20);
+    $heroHealth.text('Health' + playerChar.health);
+    $heroLog.text('Fangthar picks up a healing potion, gulps it down and feels it knit his wounds back together! he gains 20 health');
+  }
+  function playerMovement(){
+    $(`div[data-x='${playerChar.x}'][data-y='${playerChar.y}']`).removeClass('path').addClass('player');
+  }
+
+  //#################### COMBAT #####################################################//
+  const mobsArray = [
+    {health: 100, x: 3, y: 5},
+    {health: 100, x: 6, y: 2},
+    {health: 100, x: 8, y: 7},
+    {health: 100, x: 5, y: 9},
+    {health: 100, x: 7, y: 13},
+    {health: 100, x: 5, y: 23},
+    {health: 100, x: 8, y: 20},
+    {health: 100, x: 6, y: 17},
+    {health: 100, x: 2, y: 21},
+    {health: 100, x: 16, y: 25},
+    {health: 100, x: 17, y: 17},
+    {health: 100, x: 17, y: 10},
+    {health: 100, x: 11, y: 6},
+    {health: 100, x: 14, y: 1}
+  ];
+
+
+  function checkWin() {
+    if(!($('#map div').hasClass('mob'))) {
+      alert('A glowing gem falls from the lifeless corpse of the last demonic creature! Grabbing it Fangthar feels a power flow through him, drawing him...somewhere else...Fangthar holds the gem aloft and is transported to a very familar room (..In the distance an evil laugh is heard)');
+      location.reload();
+    }
+
+  }
+
+  function mobOnPlayerSquare(){
+    for(let i = 0; i < mobsArray.length; i++) {
+      const mob = mobsArray[i];
+      if(mob.x === playerChar.x && mob.y === playerChar.y) {
+        return mob;
+      }
+    }
+    null;
+  }
+  function defend() {
+    playerChar.health -= (Math.floor(Math.random() * 12));
+    $heroHealth.text('Health' + playerChar.health);
+    death(playerChar);
+  }
+  function fight(mob) {
+    mob.health -= (`${playerChar.attack}`);
+    combatAudio.play();
+    $heroLog.text(`Fangthar attacks with furious rage! Monster has ${mob.health} health left!`);
+    death(mob);
+  }
+  function death(mob){
+    if (mob.health < 1){
+      $(`div[data-x='${mob.x}'][data-y='${mob.y}']`).removeClass('mob').addClass('monsterSkeleton');
+      // const index = mobsArray.indexOf(mob);
+      $heroLog.text('Monster dies!');
+      // console.log(mobsArray[index]);
+      // mobsArray.splice(index, 1);
+      // console.log(mobsArray);
+      checkWin();
+    }if (playerChar.health < 1){
+      $heroLog.text('PLAYER DIES');
+      alert('Fanthar is overwelmed by the hordes of darkness! Reaching out he grabs hold of his amulet of time warping and travels back in time to when he first entered the dungeon!');
+      location.reload();
+    }
+  }
+
+  //############################# USER INTERFACE ##################################
+  const $heroHealth = $('#heroHealth');
+  const $heroAttack = $('#heroAttack');
+  const $heroWeapon = $('#heroWeapon');
+  const $heroLog = $('#heroLog');
+
+  $heroHealth.text('Health' + ' ' + playerChar.health);
+  $heroAttack.text('Attack' + ' ' + playerChar.attack + 'DPS');
+  $heroWeapon.text(weaponName);
+  $heroLog.text('');
+
+  //#################### WASD MOVEMENT ###############################//
+
+  function movePlayer(){
+    $(document).on('keypress', function(e){
+      switch(e.which){
+        case 119:     //PLAYER UP//
+          if (gameGrid[playerChar.x-1][playerChar.y] === 0){
+            $('.player').removeClass('player').addClass('path');
+            playerChar.x -= 1;
+            playerMovement();
+          } else if (gameGrid[playerChar.x-1][playerChar.y] === 4){
+            $('.player').removeClass('player').addClass('path');
+            playerChar.x -= 1;
+            pickupWeapon();
+          } else if (gameGrid[playerChar.x-1][playerChar.y] === 5){
+            $('.player').removeClass('player').addClass('path');
+            playerChar.x -= 1;
+            pickupHealth();
+          }if (gameGrid[playerChar.x-1][playerChar.y] === 3){
+            playerChar.x -= 1;
+            const currentMob = mobOnPlayerSquare();
+            if(currentMob) {
+              fight(currentMob);
+              defend(currentMob);
+              death(currentMob);
+            } else {
+              playerChar.x ++;
+            }
+          }
+          break;
+        case 97:     //PLAYER LEFT //
+          if (gameGrid[playerChar.x][playerChar.y-1] === 0){
+            $('.player').removeClass('player').addClass('path');
+            playerChar.y -= 1;
+            playerMovement();
+          }if (gameGrid[playerChar.x][playerChar.y-1] === 4){
+            $('.player').removeClass('player').addClass('path');
+            playerChar.y -= 1;
+            pickupWeapon();
+          }if (gameGrid[playerChar.x][playerChar.y-1] === 5){
+            $('.player').removeClass('player').addClass('path');
+            playerChar.y -= 1;
+            pickupHealth();
+          }if (gameGrid[playerChar.x][playerChar.y-1] === 3){
+            playerChar.y -= 1;
+            const currentMob = mobOnPlayerSquare();
+            if(currentMob) {
+              fight(currentMob);
+              defend(currentMob);
+              death(currentMob);
+            } else {
+              playerChar.y ++;
+            }
+          }
+          break;
+        case 115:     //PLAYER DOWN//
+          if (gameGrid[playerChar.x+1][playerChar.y] === 0){
+            $('.player').removeClass('player').addClass('path');
+            playerChar.x += 1;
+            playerMovement();
+          }if (gameGrid[playerChar.x+1][playerChar.y] === 4){
+            $('.player').removeClass('player').addClass('path');
+            playerChar.x += 1;
+            pickupWeapon();
+          }if (gameGrid[playerChar.x+1][playerChar.y] === 5){
+            $('.player').removeClass('player').addClass('path');
+            playerChar.x += 1;
+            pickupHealth();
+          }if (gameGrid[playerChar.x+1][playerChar.y] === 3){
+            playerChar.x += 1;
+            const currentMob = mobOnPlayerSquare();
+            if(currentMob) {
+              fight(currentMob);
+              defend(currentMob);
+              death(currentMob);
+            } else {
+              playerChar.x --;
+            }
+          }
+          break;
+        case 100:     //PLAYER RIGHT//
+          if (gameGrid[playerChar.x][playerChar.y+1] === 0){
+            $('.player').removeClass('player').addClass('path');
+            playerChar.y += 1;
+            playerMovement();
+          }if (gameGrid[playerChar.x][playerChar.y+1] === 4){
+            $('.player').removeClass('player').addClass('path');
+            playerChar.y += 1;
+            pickupWeapon();
+          }if (gameGrid[playerChar.x][playerChar.y+1] === 5){
+            $('.player').removeClass('player').addClass('path');
+            playerChar.y += 1;
+            pickupHealth();
+          }if (gameGrid[playerChar.x][playerChar.y+1] === 3){
+            playerChar.y += 1;
+            const currentMob = mobOnPlayerSquare();
+            if(currentMob) {
+              fight(currentMob);
+              defend(currentMob);
+              death(currentMob);
+            } else {
+              playerChar.y --;
+            }
+          }
+          break;
+      }
+    });
+  }
+  movePlayer();
+
+
+
+
+  function setup(){
+    $('#gameScreen').hide();
+    $('#intro').click(function () {
+      $('#intro').fadeOut();
+      $('#gameScreen').show();
+      const mainTheme = document.querySelector('.theme');
+      mainTheme.src = './sounds/8bit_Dungeon_Level_Video_Classica.wav';
+      mainTheme.volume = 0.1;
+      mainTheme.play();
+
+    });
+  }
+  const weaponAudio = document.querySelector('.weaponLoot');
+  weaponAudio.src = 'sounds/Metal_Parts_Cling.wav';
+  const combatAudio = document.querySelector('.combatAudio');
+  combatAudio.src = 'sounds/Slap_with_Glove.wav';
+  combatAudio.volume = 0.5;
+  const healthAudio = document.querySelector('.healthLoot');
+  healthAudio.src = 'sounds/Drink_and_Swallow.wav';
+  healthAudio.volume = 1;
+  setup();
+});
---
+++ README.md
@@ -0,0 +1,44 @@
+# Project1_Game
+
+![image](https://ga-dash.s3.amazonaws.com/production/assets/logo-9f88ae6c9c3871690e33280fcf557f33.png)
+
+# GA WDI-34 Project 1 - Fangthar's Adevnture
+For our first project, we were given a week to build an in-browser game using HTML, CSS and JavaScript. Fangthar's Adevnture is a tribute to the dungeon crawler RPG genre. I have always preferred playing RPG games, as they are great for escaping reality for a while! I decided to make a game based on games I have enjoyed playing growing up.
+
+##### [Visit website](https://delve-deep.herokuapp.com/) for best playing experience (the game was not designed for mobile).
+---
+<!-- ## Setup instructions
+- Clone or download the repo
+- Install dependencies with `yarn install`
+- Launch the app with `gulp` -->
+
+---
+# Overveiw of Fangthar's Adventure
+<p align="center"><img src="https://i.imgur.com/FHrOnu5.png" width="700"></p>
+The Game begins with an intro screen, which gives some flavour text to set scene and it doubles as instructions for the game too.
+<p align="center"><img src="https://i.imgur.com/83AHsLb.png" width="700"></p>
+Fangthar's Adevnture is set on a grid based level, hard coded into the JS file by myself. The player, monsters and items are set in place, coded into a set grid layout, allowing for more additions to the level in future.</p>
+<p align="center"><img src="https://i.imgur.com/XWk0iTP.png" width="700"></p>
+As the game progresses the player will pickup health potions and weapons to become more powerful and stay alive. Choosing which monster to attack based on your remaining health and current attack power was the aim for all of these collective features.
+
+<p align="center"><img src="https://i.imgur.com/ydSQ2Wf.png" width="700"></p>
+ Upon killing all of the monsters in the level I have  set an alert to appear, letting the player know that he/she has finished the game. I used some flavour text to try to keep the player immersed in the game.
+<p align="center"><img src="https://i.imgur.com/P0XA9Zm.png" width="700"></p>
+ IF the player should reach 0 health while attacking or going into combat with one of the monsters, then an alert comes up with some flavour text telling the player that they have failed, I set the page to reload after the user had finished reading and clicked the ok button.
+
+<br>
+
+<br>
+<h2>Things I would like to add to the game:</h2>
+ <p>I would like to find a way to create procedurally generated random maps, with random item and monster locations.</p>
+ <p>I would like to have at least 1 or 2 more enemy variants to make the game a little bit more fun and also style wise to look better.</p>
+ <p>In the future I would like to design sprites sheets specifically for the game to allow me to use more textures, as well as some animation in both the player and the
+monsters.</p>
+<p>3 Boss battles ranging from easy to hard, I wanted this feature in the game however It did not make it into the final product. The Boss images in the game are all that remain of the idea.</p>
+
+---
+# Final Thoughts
+
+I found this project to be challenging and incredibly frustrating at times, however it was incredibly rewarding to get everything to work the way i envisioned it. To take the concept of the game from the pages of my notebook to being a fully functional game which has been deployed is a great feeling.
+I am overall happy with how the game turned out, I would of liked to have bosses in the game, however i ran out of time and I wanted to get the game finished in order to style and polish the project.
+I shall be working on the game in my spare time and In doing so I hope to watch it grow with my abilities as a Developer.
